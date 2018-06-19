1f225152d03a0b21dee2b4742a0c02df93018bd4
Surbhi Bhardwaj <35597132+surbhardwaj@users.noreply.github.com>
2018-06-20 00:11:46 +0530

    Final Merging of Clusters

---
+++ Merging/src/Final_Merge.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+
+
+public class Final_Merge 
+{
+	
+	
+	 public static void main (String [] args) throws Exception
+	 {
+	     try{
+	            
+	    	 	//BufferedReader br = new BufferedReader(new FileReader("/home/surbhi/Desktop/dataset/compound.tabbed"));
+	            /*String line;
+	            line=br.readLine();
+	            while (line != null)
+	            {
+	                System.out.println(line);
+	                line=br.readLine();
+	            }*/
+	        }catch(Exception e)
+	        {
+	        	System.out.println(e);
+	        }
+	 }
+	
+	
+}
